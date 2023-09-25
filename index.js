5d8a0a96e53144b61ca5a400ec596888b8fc28e0
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-25 23:45:13 +0000

    part 2 pulling in the users location and adding in the map

--- index.js
+++ index.js
@@ -8,3 +8,49 @@ selesct interface
     insert a dropbox in html
     create a space for the map (use documentation for help)
 */
+
+/* Part 2 NOTES--------
+
+2.
+build the rest of the map by following along with leaflet startup guide
+wrap this around a funciton and use 'coords' as a paramater to replace the filler coords
+
+1. 
+receive the users coords
+
+3 and 4.
+use an addEventListener to get the data of the coordinates from the user
+    when using the addEventListener, use the submit button as a way for the querySelector
+    use the addEventListener with the 'click' as the first parameter and the searchFunction as a way to filter the results
+
+
+set the view in the code below to the users lat and long instead of the set coordinates
+    var map = L.map('map').setView([51.505, -0.09], 13);
+
+
+*/
+async function getUsersLocation() {
+    let pos = await new Promise((resolve, reject) => {
+        navigator.geolocation.getCurrentPosition(resolve, reject)
+    })
+
+    return [Math.floor(pos.coords.latitude), Math.floor(pos.coords.longitude)]
+}
+
+//replace the x and y coords with user's x and y coords.
+function createMap() { 
+    var myMap = L.map('map').setView([51.505, -0.09], 13)
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(myMap);
+}
+
+
+
+//////a function main will be at the very bottom of this code/////////
+async function main() {
+    //all the functions we created will be invoked here
+    createMap()
+}
+main()
