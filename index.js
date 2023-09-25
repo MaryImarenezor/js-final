cae29978cc2fe3c92407dd0ad0ad5084dab3c536
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-25 23:55:30 +0000

    updated index.js

--- index.js
+++ index.js
@@ -39,11 +39,11 @@ async function getUsersLocation() {
 
 //replace the x and y coords with user's x and y coords.
 function createMap() { 
-    var myMap = L.map('map').setView([51.505, -0.09], 13)
+    var map = L.map('map').setView([51.505, -0.09], 13)
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         maxZoom: 19,
         attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-    }).addTo(myMap);
+    }).addTo(map);
 }
 
 
