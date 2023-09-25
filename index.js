b0b45288617e87ab0b4d91f71dec26353d7e3204
MaryImarenezor <141180510+MaryImarenezor@users.noreply.github.com>
2023-09-25 22:56:15 +0000

    completed part 1

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
+/* Part 1 NOTES--------
+ get API from twelvedata or from any alternative api generator site
 
+for getting the user's location, adding it to the map, getting the selected location, and adding info to the map, refer to activity and documentation for help
+use geolocation to get the user's location
+
+selesct interface
+    insert a dropbox in html
+    create a space for the map (use documentation for help)
+*/
